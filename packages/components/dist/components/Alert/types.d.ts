0129d223bc8ee2a2567bd3278f1d47449ce04ea2
jinroh <robsenq@gmail.com>
2026-01-20 16:50:06 +0700

    chore: ignore build files

--- packages/components/dist/components/Alert/types.d.ts
+++
@@ -1,5 +0,0 @@
-export interface AlertMessage {
-    title?: string | null;
-    description: string;
-}
-//# sourceMappingURL=types.d.ts.map
--- packages/components/dist/composables/flyout.d.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from 'vue';
-export interface FlyoutOptions {
-    el: Ref<HTMLElement | undefined>;
-}
-export declare function useFlyout(options: FlyoutOptions): {
-    open: Readonly<Ref<boolean, boolean>>;
-    toggle: () => void;
-};
-//# sourceMappingURL=flyout.d.ts.map
